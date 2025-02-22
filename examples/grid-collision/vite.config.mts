topology-foundation/ts-topology

feat: add grid collision example
Add a new example with grid 2d where collision is implemented.
This utilizes the `resolveConflicts` CRO function for conflict resolution
on the user position.

This also reveals an issue with the `this` context within the `object`
package when `resolveConflicts` is callback from `TopologyObject`.

examples/grid-collision/vite.config.mts
@@ -1,0 +1,20 @@
+import path from "node:path";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+export default defineConfig({
+	build: {
+		target: "esnext",
+	},
+	plugins: [nodePolyfills()],
+	optimizeDeps: {
+		esbuildOptions: {
+			target: "esnext",
+		},
+	},
+	resolve: {
+		alias: {
+			"@topology-foundation": path.resolve(__dirname, "../../packages"),
+		},
+	},
+});

cwkang1998/ts-drp

examples/grid-collision/vite.config.mts
@@ -1,0 +1,20 @@
+import path from "node:path";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+export default defineConfig({
+	build: {
+		target: "esnext",
+	},
+	plugins: [nodePolyfills()],
+	optimizeDeps: {
+		esbuildOptions: {
+			target: "esnext",
+		},
+	},
+	resolve: {
+		alias: {
+			"@ts-drp": path.resolve(__dirname, "../../packages"),
+		},
+	},
+});